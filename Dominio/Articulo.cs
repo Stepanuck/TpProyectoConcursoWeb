4d27c6409b512671b744e42495c076fc4af595a4
Stepanuck <marinozzitomas@hotmail.com>
2025-10-01 00:13:25 -0300

    T3 - Bootstrap + MiMaster + Default
    Agregado de Boostrap con las herramientas descargables que ofrece la pagina oficial, en maquetado de la masterPage con el default.aspx, haciendo la primera parte, el ingreso del voucher

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -15,7 +15,7 @@ namespace Dominio
         public Marca Marca { get; set; }
         public Categoria Categoria { get; set; }
         public decimal Precio { get; set; }
-        public List<Imagen> = new List<Imagen>();
+        public List<Imagen> Imagenes { get; set; } = new List<Imagen>();
         public Articulo()
         {
             Marca = new Marca();
